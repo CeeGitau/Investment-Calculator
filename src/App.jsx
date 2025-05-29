461f9b44eb0c0c344d82c5b7a6c33b89a11aeb87
CG <96789431+CeeGitau@users.noreply.github.com>
2025-05-29 16:10:08 +0300

    Added results table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, SetUserInput] = useState({
+    initialInvestment: 15000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+        SetUserInput(prevUserInput => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: +newValue
+            };
+        });
+    }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   )
 }
